d24148f73624f3df207f4056da65d6d6f2ae6f7a
Oskar Risberg <oskar.risberg@stendahls.se>
2022-01-28 15:22:46 +0100

    Move everything around for filo

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import TarkovGunBuilder from './components/tarkov-gun-builder';
+
+import items from './items.json';
+
+function App() {
+  return (
+    <div className="App">
+      <div
+            className='page-headline-wrapper'
+        >
+            <h1>
+                Escape from Tarkov Gun Builder
+            </h1>
+        </div>
+        <TarkovGunBuilder
+            items = {items}
+        />
+    </div>
+  );
+}
+
+export default App;
