fcc094d90aa9cae85353c3bd2ecc497db854dba1
Oskar Risberg <oskar.risberg@stendahls.se>
2022-02-02 11:29:46 +0100

    update prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import TarkovGunBuilder from "./components/tarkov-gun-builder";
+import TarkovGunBuilder from './components/tarkov-gun-builder';
 
-import items from "./items.json";
+import items from './items.json';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="page-headline-wrapper">
-        <h1>Escape from Tarkov Gun Builder</h1>
-      </div>
-      <TarkovGunBuilder items={items} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="page-headline-wrapper">
+                <h1>Escape from Tarkov Gun Builder</h1>
+            </div>
+            <TarkovGunBuilder items={items} />
+        </div>
+    );
 }
 
 export default App;
