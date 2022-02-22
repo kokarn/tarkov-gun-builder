30bf063a597e6731215183e84d1490f08084c849
Oskar Risberg <oskar.risberg@stendahls.se>
2022-02-22 14:07:20 +0100

    try to load missing react

--- src/components/item-list/index.mjs
+++ src/components/item-list/index.mjs
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import usePreviousValue from '../../hooks/usePreviousValue.js';
 import './index.css';
 
--- src/components/slot/index.mjs
+++ src/components/slot/index.mjs
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
 import BackgroundImage from './assets/background.jpg';
 import ModBarrelImage from './assets/mod_barrel.png';
